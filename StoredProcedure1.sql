f1c93a520911ddbefcf938c0bb06b96fdd7ab38e
Erika Michelle Cerdas Mejias <ecerdasmejias04@gmail.com>
2023-05-20 13:42:36 -0600

    SP 2 (Productos_producidos)
    Tiene que ocurrir LOSTUPDATE

--- StoredProcedure1.sql
+++ StoredProcedure1.sql
@@ -1,7 +1,13 @@
 use caso3;
+
+IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'InsertarVentas')
+    DROP PROCEDURE InsertarVentas;
+GO
+IF EXISTS (SELECT * FROM sys.table_types WHERE name = 'VentasTVP')
+    DROP TYPE VentasTVP;
+GO
 CREATE TYPE VentasTVP AS TABLE
 (
-    venta_id INT,
     producto_id INT,
     cantidad INT,
     precioUnitario DECIMAL(10, 2),
@@ -10,6 +16,7 @@ CREATE TYPE VentasTVP AS TABLE
     tipo_cambio_id INT
 );
 GO
+
 CREATE PROCEDURE InsertarVentas
     @ventasTVP AS VentasTVP READONLY
 AS
@@ -31,8 +38,8 @@ BEGIN
 		SET @CustomError = 2001
 
 		-- Insertar ventas
-        INSERT INTO ventas (venta_id, producto_id, monto, fecha, cantidad, moneda_id, tipo_cambio_id)
-        SELECT v.venta_id, v.producto_id, (v.cantidad * v.precioUnitario * tc.tipo_Cambio), v.fecha , v.cantidad, v.moneda_id, v.tipo_cambio_id
+        INSERT INTO ventas (producto_id, monto, fecha, cantidad, moneda_id, tipo_cambio_id)
+        SELECT v.producto_id, (v.cantidad * v.precioUnitario * tc.tipo_Cambio), v.fecha, v.cantidad, v.moneda_id, v.tipo_cambio_id
         FROM @ventasTVP v
         INNER JOIN tipo_cambio tc ON v.tipo_cambio_id = tc.tipo_cambio_id AND v.moneda_id = tc.moneda_id;
 
@@ -63,17 +70,23 @@ RETURN 0
 GO
 
 -- Para hacer inserts al TVP
-/*
+
 DECLARE @misVentas AS VentasTVP;
 
 -- Rellenar la variable de tabla con los datos de venta
-INSERT INTO @misVentas (id, producto_id, cantidad, precioUnitario, fecha, moneda_id, tipo_cambio_id)
+INSERT INTO @misVentas (producto_id, cantidad, precioUnitario, fecha, moneda_id, tipo_cambio_id)
 VALUES
-    (1, 2, 5, precio, '2023-05-20', 1, 1), -- Ejemplo de venta 1
-    (2, 3, 3, precio, '2023-05-20', 2, 2); -- Ejemplo de venta 2
+    (2, 5, 510.12, '2023-05-20', 1, 1);
 
 -- Llamar al stored procedure para insertar las ventas
-EXEC InsertarVentas @misVentas;
+EXEC InsertarVentas @ventasTVP = @misVentas;
 
-*/
+
+
+/*select * from ventas
+DBCC CHECKIDENT ('ventas', RESEED, 0);
+DELETE from ventas where venta_id>0;*/
+
+
+
 
--- StoredProcedure2.sql
+++ StoredProcedure2.sql
@@ -1,1 +1,80 @@
 use caso3;
+IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'ProducirProductos')
+    DROP PROCEDURE ProducirProductos;
+GO
+IF EXISTS (SELECT * FROM sys.table_types WHERE name = 'ProductosTVP')
+    DROP TYPE ProductosTVP;
+GO
+
+CREATE TYPE ProductosTVP AS TABLE
+(
+    cantidad INT,
+    posttime DATETIME,
+    user_id SMALLINT,
+    producto_id SMALLINT
+    --contrato_id SMALLINT
+);
+GO
+
+CREATE PROCEDURE ProducirProductos
+    @productosTVP AS ProductosTVP READONLY
+AS
+BEGIN
+    SET NOCOUNT ON -- no retorne metadatos
+
+	DECLARE @ErrorNumber INT, @ErrorSeverity INT, @ErrorState INT, @CustomError INT
+	DECLARE @Message VARCHAR(200)
+	DECLARE @InicieTransaccion BIT
+
+	SET @InicieTransaccion = 0
+	IF @@TRANCOUNT=0 BEGIN
+		SET @InicieTransaccion = 1
+		SET TRANSACTION ISOLATION LEVEL READ COMMITTED
+		BEGIN TRANSACTION
+	END
+
+	BEGIN TRY
+		SET @CustomError = 2001
+
+        UPDATE pp
+        SET cantidad = pp.cantidad + p.cantidad
+        FROM productos_producidos pp
+        INNER JOIN @productosTVP p ON pp.producto_id = p.producto_id /*and pp.contrato_id = p.contrato_id*/;
+
+		IF @InicieTransaccion=1 BEGIN
+			COMMIT
+		END
+	END TRY
+	BEGIN CATCH
+		SET @ErrorNumber = ERROR_NUMBER()
+		SET @ErrorSeverity = ERROR_SEVERITY()
+		SET @ErrorState = ERROR_STATE()
+		SET @Message = ERROR_MESSAGE()
+
+		IF @InicieTransaccion=1 BEGIN
+			ROLLBACK
+		END
+		RAISERROR('%s - Error Number: %i',
+			@ErrorSeverity, @ErrorState, @Message, @CustomError)
+	END CATCH
+END
+RETURN 0
+GO
+
+-- Para hacer inserts al TVP
+
+DECLARE @misProductos AS ProductosTVP;
+
+-- Rellenar la variable de tabla con los datos de venta
+INSERT INTO @misProductos (cantidad, posttime, user_id, producto_id)
+VALUES
+    (32,'2023-05-20', 2, 2);
+
+-- Llamar al stored procedure para insertar las ventas
+EXEC ProducirProductos @productosTVP = @misProductos;
+
+
+
+/*select * from productos_producidos
+DBCC CHECKIDENT ('ventas', RESEED, 0);
+DELETE from ventas where venta_id>0;*/
